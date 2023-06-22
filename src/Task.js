6547eaa9e2ace94f06a24bcabb12c647da67e654
amandaslwa <meluatke@gmail.com>
2023-06-22 18:19:40 +0700

    First Commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Amanda Salwa Salsabila" // TODO: replace this
+const studentId = "FE4437780" // TODO: replace this
+
+export { url, studentName, studentId }
